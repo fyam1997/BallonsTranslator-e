2130f43fee492a13b477c6dad1f7c780cc510609
fyam <fung19971115@gmail.com>
2025-10-23 21:11:30 +0800

    rearrange method

--- utils/clone_translation.py
+++ utils/clone_translation.py
@@ -12,38 +12,40 @@ def clone_translations(src, target):
     target_dir = os.path.expanduser(target)
     src_proj = ProjImgTrans(src_dir)
     target_proj = ProjImgTrans(target_dir)
-
     for src_key, target_key in zip(src_proj.pages, target_proj.pages):
-        # Clone project json
-        target_proj.pages[target_key] = src_proj.pages[src_key]
-
-        # Clone masks
-        shutil.copy(
-            src_proj.get_mask_path(src_key),
-            target_proj.get_mask_path(target_key),
-        )
-
-        # Clone inpainted images
-        src_raw_img = src_proj.read_img(src_key)
-        target_raw_img = src_proj.read_img(src_key)
-        src_inpainted_img = src_proj.load_inpainted_by_imgname(src_key)
-        clone_rendered(
-            src_img=src_raw_img,
-            rendered_img=src_inpainted_img,
-            target_img=target_raw_img,
-            save_path=target_proj.get_inpainted_path(target_key),
-        )
-
-        # Clone result images
-        src_result_img = imread(src_proj.get_result_path(src_key))
-        clone_rendered(
-            src_img=src_raw_img,
-            rendered_img=src_result_img,
-            target_img=target_raw_img,
-            save_path=target_proj.get_result_path(target_key),
-        )
-
+        clone_page(src_proj, target_proj, src_key, target_key)
     target_proj.save()
+
+
+def clone_page(src_proj, target_proj, src_key, target_key):
+    # Clone project json
+    target_proj.pages[target_key] = src_proj.pages[src_key]
+
+    # Clone masks
+    shutil.copy(
+        src_proj.get_mask_path(src_key),
+        target_proj.get_mask_path(target_key),
+    )
+
+    # Clone inpainted images
+    src_raw_img = src_proj.read_img(src_key)
+    target_raw_img = src_proj.read_img(src_key)
+    src_inpainted_img = src_proj.load_inpainted_by_imgname(src_key)
+    clone_rendered(
+        src_img=src_raw_img,
+        rendered_img=src_inpainted_img,
+        target_img=target_raw_img,
+        save_path=target_proj.get_inpainted_path(target_key),
+    )
+
+    # Clone result images
+    src_result_img = imread(src_proj.get_result_path(src_key))
+    clone_rendered(
+        src_img=src_raw_img,
+        rendered_img=src_result_img,
+        target_img=target_raw_img,
+        save_path=target_proj.get_result_path(target_key),
+    )
 
 
 def clone_rendered(src_img, rendered_img, target_img, save_path):
